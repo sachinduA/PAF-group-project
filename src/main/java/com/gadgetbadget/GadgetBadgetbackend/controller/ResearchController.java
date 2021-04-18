3d1bcfcdffa29a089ae9c4881635afbec730d1b7
Diliru <diliru.c95@gmail.com>
2021-04-18 17:32:28 +0530

    Create JPA relationships

--- src/main/java/com/gadgetbadget/GadgetBadgetbackend/controller/ResearchController.java
+++ src/main/java/com/gadgetbadget/GadgetBadgetbackend/controller/ResearchController.java
@@ -14,13 +14,33 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gadgetbadget.GadgetBadgetbackend.model.Buyer;
+import com.gadgetbadget.GadgetBadgetbackend.model.Funder;
 import com.gadgetbadget.GadgetBadgetbackend.model.Research;
+import com.gadgetbadget.GadgetBadgetbackend.model.Researcher;
+import com.gadgetbadget.GadgetBadgetbackend.model.Supervisor;
+import com.gadgetbadget.GadgetBadgetbackend.service.BuyerService;
+import com.gadgetbadget.GadgetBadgetbackend.service.FunderService;
 import com.gadgetbadget.GadgetBadgetbackend.service.ResearchService;
+import com.gadgetbadget.GadgetBadgetbackend.service.ResearcherService;
+import com.gadgetbadget.GadgetBadgetbackend.service.SupervisorService;
 
 @RestController
 public class ResearchController {
 	@Autowired
 	private ResearchService service;
+
+	@Autowired
+	private ResearcherService researcherService;
+
+	@Autowired
+	private BuyerService buyerService;
+
+	@Autowired
+	private FunderService funderService;
+
+	@Autowired
+	private SupervisorService supervisorService;
 
 	@GetMapping("/researches")
 	public List<Research> getAll() {
@@ -38,10 +58,8 @@ public class ResearchController {
 	}
 
 	@PostMapping("/researches")
-	public ResponseEntity<Research> add(@RequestBody Research research) {
-		System.out.println(research);
+	public void add(@RequestBody Research research) {
 		service.save(research);
-		return new ResponseEntity<Research>(research, HttpStatus.OK);
 	}
 
 	@PutMapping("/researches/{id}")
@@ -49,7 +67,7 @@ public class ResearchController {
 		try {
 			@SuppressWarnings("unused")
 			Research existResearcher = service.get(id);
-			research.setId(id);
+			research.setResearch_id(id);
 			service.save(research);
 			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (NoSuchElementException e) {
@@ -60,5 +78,76 @@ public class ResearchController {
 	@DeleteMapping("/researches/{id}")
 	public void delete(@PathVariable Integer id) {
 		service.delete(id);
+	}
+
+	@PutMapping("researches/{id}/researchers/{researcherId}")
+	public void addResearcherToResearch(@PathVariable int id, @PathVariable int researcherId) {
+		Research research = service.get(id);
+		Researcher researcher = researcherService.get(researcherId);
+
+		research.getResearchers().add(researcher);
+		service.save(research);
+	}
+
+	@DeleteMapping("researches/{id}/researchers/{researcherId}")
+	public void deleteResearcherFromResearch(@PathVariable int id, @PathVariable int researcherId) {
+		Research research = service.get(id);
+		Researcher researcher = researcherService.get(researcherId);
+
+		research.getResearchers().remove(researcher);
+		service.save(research);
+	}
+
+	@PutMapping("researches/{id}/buyers/{buyerId}")
+	public void addBuyerToResearch(@PathVariable int id, @PathVariable int buyerId) {
+		Research research = service.get(id);
+		Buyer buyer = buyerService.get(buyerId);
+
+		research.getBuyers().add(buyer);
+		service.save(research);
+	}
+
+	@DeleteMapping("researches/{id}/buyers/{buyerId}")
+	public void deleteBuyerFromResearch(@PathVariable int id, @PathVariable int buyerId) {
+		Research research = service.get(id);
+		Buyer buyer = buyerService.get(buyerId);
+
+		research.getBuyers().remove(buyer);
+		service.save(research);
+	}
+
+	@PutMapping("researches/{id}/funders/{funderId}")
+	public void addFunderToResearch(@PathVariable int id, @PathVariable int funderId) {
+		Research research = service.get(id);
+		Funder funder = funderService.get(funderId);
+
+		research.getFunders().add(funder);
+		service.save(research);
+	}
+
+	@DeleteMapping("researches/{id}/funders/{funderId}")
+	public void deleteFunderFromResearch(@PathVariable int id, @PathVariable int funderId) {
+		Research research = service.get(id);
+		Funder funder = funderService.get(funderId);
+
+		research.getFunders().remove(funder);
+		service.save(research);
+	}
+
+	@PutMapping("researches/{id}/supervisors/{supervisorId}")
+	public void addSupervisorToResearch(@PathVariable int id, @PathVariable int supervisorId) {
+		Research research = service.get(id);
+		Supervisor supervisor = supervisorService.get(supervisorId);
+
+		research.setSupervisor(supervisor);
+		service.save(research);
+	}
+
+	@DeleteMapping("researches/{id}/supervisors")
+	public void removeSupervisorFromResearch(@PathVariable int id) {
+		Research research = service.get(id);
+
+		research.setSupervisor(null);
+		service.save(research);
 	}
 }
--- src/main/java/com/gadgetbadget/GadgetBadgetbackend/model/Research.java
+++ src/main/java/com/gadgetbadget/GadgetBadgetbackend/model/Research.java
@@ -1,43 +1,63 @@
 package com.gadgetbadget.GadgetBadgetbackend.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "researches")
 public class Research {
-
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int research_id;
 	private String name;
 	private String description;
 	private String category;
 
-	//@ManyToMany(mappedBy = "researches")
-	//private Set<Researcher> researchers = new HashSet<>();
+	@ManyToMany()
+	@JoinTable(name = "research_management", joinColumns = @JoinColumn(name = "research_id"), inverseJoinColumns = @JoinColumn(name = "researcher_id"))
+	private Set<Researcher> researchers = new HashSet<>();
+
+	@ManyToMany()
+	@JoinTable(name = "research_buyings", joinColumns = @JoinColumn(name = "research_id"), inverseJoinColumns = @JoinColumn(name = "buyer_id"))
+	private Set<Buyer> buyers = new HashSet<>();
+
+	@ManyToMany()
+	@JoinTable(name = "research_fundings", joinColumns = @JoinColumn(name = "research_id"), inverseJoinColumns = @JoinColumn(name = "funder_id"))
+	private Set<Funder> funders = new HashSet<>();
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "supervisor_id")
+	private Supervisor supervisor;
 
 	public Research() {
 
 	}
 
-	public Research(int id, String name, String description, String category) {
+	public Research(int research_id, String name, String description, String category) {
 		super();
-		this.id = id;
+		this.research_id = research_id;
 		this.name = name;
 		this.description = description;
 		this.category = category;
 	}
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public int getId() {
-		return id;
+	public int getResearch_id() {
+		return research_id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setResearch_id(int research_id) {
+		this.research_id = research_id;
 	}
 
 	public String getName() {
@@ -63,4 +83,37 @@ public class Research {
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
+	public Set<Researcher> getResearchers() {
+		return researchers;
+	}
+
+	public void setResearchers(Set<Researcher> researchers) {
+		this.researchers = researchers;
+	}
+
+	public Set<Buyer> getBuyers() {
+		return buyers;
+	}
+
+	public void setBuyers(Set<Buyer> buyers) {
+		this.buyers = buyers;
+	}
+
+	public Set<Funder> getFunders() {
+		return funders;
+	}
+
+	public void setFunders(Set<Funder> funders) {
+		this.funders = funders;
+	}
+
+	public Supervisor getSupervisor() {
+		return supervisor;
+	}
+
+	public void setSupervisor(Supervisor supervisor) {
+		this.supervisor = supervisor;
+	}
+
 }
