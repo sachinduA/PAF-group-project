da1da9fe61c6e40f39940722e60df89f0b116604
Hasantha <hasantha9422@gmail.com>
2021-04-21 15:07:15 +0530

    Created interfaces

--- src/main/java/com/gadgetbadget/GadgetBadgetbackend/controller/SupervisorController.java
+++ src/main/java/com/gadgetbadget/GadgetBadgetbackend/controller/SupervisorController.java
@@ -6,8 +6,11 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,16 +21,81 @@ import com.gadgetbadget.GadgetBadgetbackend.model.Supervisor;
 import com.gadgetbadget.GadgetBadgetbackend.service.SupervisorService;
 
 @RestController
+@Controller
 public class SupervisorController {
 	@Autowired
 	private SupervisorService service;
 
+	// View Supervisors
 	@GetMapping("/supervisors")
+	public String getAll(Model model) {
+		model.addAttribute("supervisors", service.getAll());
+		return "supervisors";
+	}
+
+	@GetMapping("/supervisors/{id}")
+	public String get(@PathVariable Integer id, Model model) {
+		try {
+			Supervisor supervisor = service.get(id);
+			model.addAttribute("supervisor", supervisor);
+			return "supervisor";
+		} catch (NoSuchElementException e) {
+			model.addAttribute("error", "Supervisor not available");
+			return "supervisor";
+		}
+	}
+
+	// Add new Supervisor
+	@GetMapping("/newSupervisorForm")
+	public String newSupervisorForm(Model model) {
+		Supervisor supervisor = new Supervisor();
+		model.addAttribute("supervisor", supervisor);
+		return "supervisorNew";
+	}
+
+	@PostMapping("/newSupervisor")
+	public String newSupervisor(@ModelAttribute("supervisor") Supervisor supervisor) {
+		service.save(supervisor);
+		return "redirect:/supervisors";
+	}
+
+	// Update Supervisor
+	@GetMapping("/updateSupervisorForm/{id}")
+	public String updateSupervisorForm(@PathVariable int id, Model model) {
+		Supervisor supervisor = service.get(id);
+		model.addAttribute("supervisor", supervisor);
+		return "supervisorUpdate";
+	}
+
+	@PostMapping("/updateSupervisorForm/updateSupervisor")
+	public String updateSupervisor(@ModelAttribute("supervisor") Supervisor supervisor) {
+		try {
+			Supervisor existingSupervisor = service.get(supervisor.getSupervisor_id());
+			supervisor.setSupervisor_id(existingSupervisor.getSupervisor_id());
+			service.save(supervisor);
+			return "redirect:/supervisors";
+		} catch (NoSuchElementException e) {
+			return "redirect:/supervisors";
+		}
+	}
+
+	@GetMapping("/deleteSupervisor/{id}")
+	public String deleteSupervisor(@PathVariable Integer id) {
+		service.delete(id);
+		return "redirect:/supervisors";
+	}
+	
+	/* ------------------------------------------
+	 * REST API Endpoints
+	 * ------------------------------------------
+	 */
+	
+	@GetMapping("/get-supervisors")
 	public List<Supervisor> getAll() {
 		return service.getAll();
 	}
 
-	@GetMapping("/supervisors/{id}")
+	@GetMapping("/get-supervisor/{id}")
 	public ResponseEntity<Supervisor> get(@PathVariable Integer id) {
 		try {
 			Supervisor supervisor= service.get(id);
@@ -37,12 +105,12 @@ public class SupervisorController {
 		}
 	}
 
-	@PostMapping("/supervisors")
+	@PostMapping("/new-supervisor")
 	public void add(@RequestBody Supervisor supervisor) {
 		service.save(supervisor);
 	}
 
-	@PutMapping("/supervisors/{id}")
+	@PutMapping("/update-supervisor/{id}")
 	public ResponseEntity<?> update(@RequestBody Supervisor supervisor, @PathVariable Integer id) {
 		try {
 			@SuppressWarnings("unused")
@@ -55,7 +123,7 @@ public class SupervisorController {
 		}
 	}
 
-	@DeleteMapping("/supervisors/{id}")
+	@DeleteMapping("/delete-supervisor/{id}")
 	public void delete(@PathVariable Integer id) {
 		service.delete(id);
 	}
