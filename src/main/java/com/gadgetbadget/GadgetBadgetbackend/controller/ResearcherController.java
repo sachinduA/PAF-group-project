59cac4ffa9f1b1997a1a4100472369ad2d590c04
Hansi <hansirathnayaka493@gmail.com>
2021-04-21 15:07:51 +0530

    Created interfaces

--- src/main/java/com/gadgetbadget/GadgetBadgetbackend/controller/ResearcherController.java
+++ src/main/java/com/gadgetbadget/GadgetBadgetbackend/controller/ResearcherController.java
@@ -6,8 +6,11 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,16 +21,81 @@ import com.gadgetbadget.GadgetBadgetbackend.model.Researcher;
 import com.gadgetbadget.GadgetBadgetbackend.service.ResearcherService;
 
 @RestController
+@Controller
 public class ResearcherController {
 	@Autowired
 	private ResearcherService service;
 
 	@GetMapping("/researchers")
+	public String getAll(Model model) {
+		model.addAttribute("researchers", service.getAll());
+		return "researchers";
+	}
+
+	@GetMapping("/researchers/{id}")
+	public String get(@PathVariable Integer id, Model model) {
+		try {
+			Researcher researcher = service.get(id);
+			model.addAttribute("researcher", researcher);
+			return "researcher";
+		} catch (NoSuchElementException e) {
+			model.addAttribute("error", "Researcher is not available.");
+			return "researcher";
+		}
+	}
+
+	// Add new Researcher
+	@GetMapping("/newResearcherForm")
+	public String newResearcherForm(Model model) {
+		Researcher researcher = new Researcher();
+		model.addAttribute("researcher", researcher);
+		return "researcherNew";
+	}
+
+	@PostMapping("/newResearcher")
+	public String newResearcher(@ModelAttribute("researcher") Researcher researcher, Model model) {
+		service.save(researcher);
+		model.addAttribute("researcher", researcher);
+		return "redirect:/researchers";
+	}
+
+	// Update Researcher
+	@GetMapping("/updateResearcherForm/{id}")
+	public String updateResearcherForm(@PathVariable int id, Model model) {
+		Researcher researcher = service.get(id);
+		model.addAttribute("researcher", researcher);
+		return "researcherUpdate";
+	}
+
+	@PostMapping("/updateResearcherForm/updateResearcher")
+	public String updateResearcher(@ModelAttribute("researcher") Researcher researcher) {
+		try {
+			Researcher existingResearcher = service.get(researcher.getResearcher_id());
+			researcher.setResearcher_id(existingResearcher.getResearcher_id());
+			service.save(researcher);
+			return "redirect:/researchers";
+		} catch (NoSuchElementException e) {
+			return "redirect:/researchers";
+		}
+	}
+
+	@GetMapping("/deleteResearcher/{id}")
+	public String deleteResearcher(@PathVariable Integer id) {
+		service.delete(id);
+		return "redirect:/researchers";
+	}
+	
+	/* ------------------------------------------
+	 * REST API Endpoints
+	 * ------------------------------------------
+	 */
+	
+	@GetMapping("/get-researchers")
 	public List<Researcher> getAll() {
 		return service.getAll();
 	}
 
-	@GetMapping("/researchers/{id}")
+	@GetMapping("/get-researcher/{id}")
 	public ResponseEntity<Researcher> get(@PathVariable Integer id) {
 		try {
 			Researcher researcher = service.get(id);
@@ -37,12 +105,12 @@ public class ResearcherController {
 		}
 	}
 
-	@PostMapping("/researchers")
+	@PostMapping("/new-researcher")
 	public void add(@RequestBody Researcher researcher) {
 		service.save(researcher);
 	}
 
-	@PutMapping("/researchers/{id}")
+	@PutMapping("/update-researcher/{id}")
 	public ResponseEntity<?> update(@RequestBody Researcher researcher, @PathVariable Integer id) {
 		try {
 			@SuppressWarnings("unused")
@@ -55,7 +123,7 @@ public class ResearcherController {
 		}
 	}
 
-	@DeleteMapping("/researchers/{id}")
+	@DeleteMapping("/delete-researcher/{id}")
 	public void delete(@PathVariable Integer id) {
 		service.delete(id);
 	}
